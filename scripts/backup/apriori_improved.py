30d8e36478c65231dae5e15e4f8102dc578b2f3d
phucn3101 <65848937+phucn3101@users.noreply.github.com>
2024-07-25 01:27:42 +0700

    Delete scripts/backup directory

--- scripts/backup/apriori_improved.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from mlxtend.frequent_patterns import apriori, association_rules
-
-def load_data(file_path):
-    data = pd.read_csv(file_path)
-    return data
-
-# not done yet
--- scripts/backup/fpgrowth_improved.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from mlxtend.frequent_patterns import fpgrowth, association_rules
-
-def load_data(file_path):
-    data = pd.read_csv(file_path)
-    return data
-
-# not done yet
